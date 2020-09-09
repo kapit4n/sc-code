769a1f611ac029d30fcb07300bd6f73aed450117
Luis Arce <luis.arce22@gmail.com>
2020-09-08 20:05:56 -0400

    iterative binary search

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,12 +5,19 @@ object Main {
 
     var s = new Search();
     var index = s.binarySearch(Array(1, 2, 5, 6, 8, 10), 6)(0, 6)
+    var index2 = s.binarySearchIt(Array(1, 2, 5, 6, 8, 10), 6)
 
     if (index == -1){
       println("Not found")
     } else {
       println("Element is on the index " + index)
     }
-    println("Hello world")
+
+    if (index == index2) {
+      println("Passed")
+    } else {
+      println("error to make search the item")
+    }
+
   }
 }
--- src/main/scala/Search.scala
+++ src/main/scala/Search.scala
@@ -1,6 +1,8 @@
 package main.scala
 
 class Search {
+
+  // use recursion
   def binarySearch(arr: Array[Int], toSearch: Int)(low: Int = 0, high: Int = arr.length - 1): Int = {
     if (low > high) return -1
 
@@ -13,5 +15,24 @@ class Search {
     }else {
       return binarySearch(arr, toSearch)(middle + 1, high)
     }
+  }
+
+  // use a loop to search the item
+  def binarySearchIt(arr: Array[Int], toSearch: Int): Int = {
+    var low = 0
+    var high = arr.length
+
+    while(low <= high) {
+      var middle = low + (high - low) / 2
+      if (arr(middle) == toSearch) {
+        return middle
+      } else if (arr(middle) < toSearch) {
+        high = middle - 1
+      } else {
+        low = middle + 1
+      }
+    }
+
+    -1
   }
 }
